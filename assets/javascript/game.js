aff24cc62f491642084dd955e57c2144407c2841
Joey2Jobs <scholz.joe@gmail.com>
2018-12-10 22:30:50 -0600

    notFunctioningFunctionForFirstGem

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,20 +6,29 @@ var redGem;
 var yellowGem;
 var purpleGem;
 
-var userScore;
+var userScore = 0;
 
 var gameNum;
 //120-19+1=102
 gameNumber = Math.floor(Math.random() * 102) + 19
-console.log(gameNum);
+console.log(gameNumber);
 
 $("#gameNum").html(gameNumber);
 
-var crystalNum;
-crystalNum = Math.floor(Math.random() * 12) + 1
-console.log(crystalNum)
+// // var crystalNum;
+// crystalNum = Math.floor(Math.random() * 12) + 1
 
-$(".green").on("click" greenGem) + (crystalNum));
+var greenGem = Math.floor(Math.random() * 12) + 1
+console.log(greenGem)
+
+$(".green").on("click", function () {
+userScore += greenGem;
+console.log("totalUserScore " + greenGem)
+});
+
+
+
+
 // random number for gameNum 19-120
 // random number for crystalNum 1-12 x 4
 // scoreCount
